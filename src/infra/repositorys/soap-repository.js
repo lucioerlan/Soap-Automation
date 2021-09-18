ee825cd5bb7478ec7ee79515300a43d628f3b8f9
Erlan Lucio  <67064886+lucioerlan@users.noreply.github.com>
2021-09-18 16:22:52 -0300

    fix sonar smells

--- src/infra/repositorys/soap-repository.js
+++ src/infra/repositorys/soap-repository.js
@@ -3,9 +3,9 @@ const pg_Conn = require('../database/connection');
 module.exports = new class SoapRepository {
     getAddress() {
         return pg_Conn('andress').select('cep');
-    };
+    }
 
     updateAddress(address) {
         return pg_Conn('andress').update(address).where('cep', address.cep);
-    };
+    }
 };
